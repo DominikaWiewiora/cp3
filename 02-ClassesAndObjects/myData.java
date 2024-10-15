a620d640cf08afd06c6c4b8ca81ad8855813aef6
Dominika Wiewióra <146983959+DominikaWiewiora@users.noreply.github.com>
2024-10-15 22:45:36 +0200

    Add files via upload

---
+++ 02-ClassesAndObjects/myData.java
@@ -0,0 +1,21 @@
+public class myData {
+
+    public static void main(String[] args) {
+        String myName = "Dominika";
+        String mySurname = "Wiewióra";
+        int myAge = 20;
+        int myId = 231243;
+        boolean iAmFirstYearStudent = false;
+        String myFieldOfStudy = "Applied Informatics";
+        String myUniversity = "Cracow University of Economics";
+
+        System.out.println("My name: "+myName);
+        System.out.println("My surname: "+mySurname);
+        System.out.println("My age: "+myAge);
+        System.out.println("My student ID: "+myId);
+        System.out.println("Am I first year student?: "+iAmFirstYearStudent);
+        System.out.println("My field of study: "+myFieldOfStudy);
+        System.out.println("My University: "+myUniversity);
+
+    }
+}
